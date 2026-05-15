dafb0662516145a5e4b76a94e0c835bba69517c7
fernando <fergolde@gmail.com>
2026-05-15 19:10:36 +0200

    fix: reset database to version 1

--- app/src/main/java/com/example/velodrome/data/local/VelodromeDatabase.kt
+++ app/src/main/java/com/example/velodrome/data/local/VelodromeDatabase.kt
@@ -3,7 +3,6 @@ package com.example.velodrome.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.velodrome.data.local.dao.AlbumDao
 import com.example.velodrome.data.local.dao.ArtistDao
 import com.example.velodrome.data.local.dao.ScrobbleDao
@@ -15,7 +14,7 @@ import com.example.velodrome.data.local.entity.TrackEntity
 
 @Database(
     entities = [ArtistEntity::class, AlbumEntity::class, TrackEntity::class, ScrobbleEntity::class],
-    version = 2,
+    version = 1,
     exportSchema = false
 )
 abstract class VelodromeDatabase : RoomDatabase() {
@@ -26,11 +25,5 @@ abstract class VelodromeDatabase : RoomDatabase() {
 
     companion object {
         const val DATABASE_NAME = "velodrome_db"
-    }
-}
-
-val MIGRATION_1_2 = object : Migration(1, 2) {
-    override fun migrate(db: SupportSQLiteDatabase) {
-        db.execSQL("ALTER TABLE tracks ADD COLUMN localFilePath TEXT DEFAULT NULL")
     }
 }
