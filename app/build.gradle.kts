24e44c3f5f88a710917c7b4b7d2bd26c83ed9b41
fernando <fergolde@gmail.com>
2026-08-24 15:24:27 +0200

    chore(release): bump version to 2.5.7 (versionCode 31)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.fergolde.velodrome"
         minSdk = 34
         targetSdk = 37
-        versionCode = 30
-        versionName = "2.5.6"
+        versionCode = 31
+        versionName = "2.5.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
