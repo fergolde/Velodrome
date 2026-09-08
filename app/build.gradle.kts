81387418aacd51010e54935fcdc31834894b272d
fernando <fergolde@gmail.com>
2026-09-08 11:50:13 +0200

    chore(release): bump version to 2.6.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 34
         targetSdk = 37
         versionCode = 35
-        versionName = "2.6.5"
+        versionName = "2.6.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
